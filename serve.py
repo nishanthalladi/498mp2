a6c3a839f53183a9247d7231a537466bb2f0791a
Ubuntu <ubuntu@ip-172-31-13-34.us-east-2.compute.internal>
2024-01-29 01:52:59 +0000

    init

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def serve():
+    if request.method == 'POST':
+        subprocess.Popen(["python3", "stress_cpu.py"]) 
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+    if request.method == 'GET':
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
